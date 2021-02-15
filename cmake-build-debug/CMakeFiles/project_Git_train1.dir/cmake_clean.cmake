20ca100ff91325e7b6dc2274d6bdfa7f3173f809
Jakub <kubator222@wp.pl>
2021-02-15 13:18:31 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/project_Git_train1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_Git_train1.dir/main.cpp.obj"
+  "libproject_Git_train1.dll.a"
+  "project_Git_train1.exe"
+  "project_Git_train1.exe.manifest"
+  "project_Git_train1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_Git_train1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/project_Git_train1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for project_Git_train1.
+# This may be replaced when dependencies are built.
